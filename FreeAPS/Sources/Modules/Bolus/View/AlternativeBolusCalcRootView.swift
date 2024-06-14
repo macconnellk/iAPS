961d87ae669068eca8e51965c2e7be030eb05554
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-06-14 14:07:00 -0400

    Update AlternativeBolusCalcRootView.swift

--- FreeAPS/Sources/Modules/Bolus/View/AlternativeBolusCalcRootView.swift
+++ FreeAPS/Sources/Modules/Bolus/View/AlternativeBolusCalcRootView.swift
@@ -98,16 +98,21 @@ extension Bolus {
                                 .onChange(of: state.useFattyMealCorrectionFactor) { _ in 
                                     //If new carbs have been entered in the last 3 minutes
                                     if let firstMeal = meal.first,
-                                       let createdAt = firstMeal.createdAt,
-                                       Date().timeIntervalSince(createdAt) < 180 {      // 180 seconds = 3 minutes
-                                       if let carbs2 = firstMeal.carbs, carbs2 > 0 {
-                                            state.insulinCalculated = state.calculateInsulin(carbs2: Decimal(carbs2))
-                                          } 
-                                        } else {
-                                       let carbs2 = 0
-                                        state.insulinCalculated = state.calculateInsulin(carbs2: Decimal(carbs2))
+                                           let createdAt = firstMeal.createdAt,
+                                           Date().timeIntervalSince(createdAt) < 180 {      // 180 seconds = 3 minutes
+                                               if let carbs2 = firstMeal.carbs, carbs2 > 0 {
+                                                state.insulinCalculated = state.calculateInsulin(carbs2: Decimal(carbs2))
+                                              } 
+                                               else {
+                                                   let carbs2 = 0
+                                                state.insulinCalculated = state.calculateInsulin(carbs2: Decimal(carbs2))
+                                                } 
+                                       } 
+                                        else {
+                                               let carbs2 = 0
+                                                state.insulinCalculated = state.calculateInsulin(carbs2: Decimal(carbs2))
                                         } 
-                                }
+                                 
                             }
                         }
 
